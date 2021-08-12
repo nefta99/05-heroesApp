e0c52f427b91b4ad38d5a0d05fb4b21857232857
nefta99 <israelneftali@gmail.com>
2021-08-12 16:46:36 -0500

    video : 170

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+//Comentario
 @NgModule({
   declarations: [
     AppComponent
