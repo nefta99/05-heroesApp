32e346023a37a80ec31fa9471803428dee3ab1aa
nefta99 <israelneftali@gmail.com>
2021-08-13 09:46:01 -0500

    video : 175

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//modulos
+import { HeroesRouterModule } from './heroes-router.module';
+
 //Componentes personales
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+
 
 
 
@@ -19,7 +23,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
     ListadoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HeroesRouterModule
   ]
 })
 export class HeroesModule { }
