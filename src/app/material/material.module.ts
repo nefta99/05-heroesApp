0dad842029c9effbf5fd2d6c064d401b21bf2769
nefta99 <israelneftali@gmail.com>
2021-08-16 15:59:59 -0500

    video:  187

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,13 +4,16 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';  
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 
 @NgModule({
   exports:[
     MatButtonModule,
+    MatCardModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,    
