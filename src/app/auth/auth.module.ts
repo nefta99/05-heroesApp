43439748d5e04b7d7a9b8f5b888624a51731f0b0
nefta99 <israelneftali@gmail.com>
2021-08-20 19:37:26 -0500

    video : 218

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
 //modulos
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -10,15 +11,19 @@ import { RegistroComponent } from './pages/registro/registro.component';
 
 
 
+
 @NgModule({
   declarations: [
     LoginComponent,
+    
     RegistroComponent
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     //Este siempre tiene que estar aqui  para que funcionen las rutas
     AuthRoutingModule
+    
   ]
 })
 export class AuthModule { }
