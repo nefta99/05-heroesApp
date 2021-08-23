b6d63bd9ba758d9b68bd6469ca6dcb23e133f74a
nefta99 <israelneftali@gmail.com>
2021-08-23 10:19:46 -0500

    video : 219

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,7 @@
+
 import { Component} from '@angular/core';
 import {  Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +11,22 @@ import {  Router } from '@angular/router';
 })
 export class LoginComponent  {
 
-  constructor(private router : Router) { }
+  constructor(private router : Router,
+      private authservice :AuthService
+    ) { }
 
 
   login(){
     // ir al backend
     //un usuario
-    this.router.navigate(['./heroes']);
+
+
+    this.authservice.login().subscribe(resp=> {
+      console.log(resp);
+      if(resp.id){
+        this.router.navigate(['./heroes']);
+      }
+    });
+    // 
   }
 }
