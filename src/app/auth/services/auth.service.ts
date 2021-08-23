e237e70c3346fb23f92ca595595a3cc27763775f
nefta99 <israelneftali@gmail.com>
2021-08-23 11:07:52 -0500

    video : 220

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -10,12 +11,24 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseUrl : string = environment.baseUrl;
+  private _auth : Auth | undefined;
 
+  get auth() : Auth{
+    //Con esto {...objeto} hacemos que el objeto no pase por referencia es decir
+    //que no se pueda modificar el contenido del objeto por ningun lado para que no cambien los valores
+
+    return {...this._auth!}
+  }
 
   constructor(private http : HttpClient) { }
 
+  //Esto esta dentro del services
   login(){
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+    .pipe(
+      //Esto se ejecuta primero que el subcribe que cosume el servicio
+      tap(resp => this._auth = resp)
+    )
   }
 
 
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +15,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router :Router) { }
+  get auth(){
+    return this.authservice.auth;
+  }
+  constructor(private router :Router, 
+      private authservice :AuthService
+    ) { }
 
   ngOnInit(): void {
   }
